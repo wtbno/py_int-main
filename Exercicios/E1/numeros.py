983f0b2cd047a9851e6e14bb8daed498c8458d3c
wtbno <bruno_links@hotmail.com>
2022-12-03 15:47:23 -0300

    continuação

---
+++ Exercicios/E1/numeros.py
@@ -0,0 +1,6 @@
+num1 = int(input('Insira o primeiro valor '))
+num2 = int(input('Insira o segundo número: '))
+soma = num1 + num2
+print('A soma entre {}  e {} vale {}'.format(num1, num2, soma))
+# {} é uma máscara que irá ter um os valores de .format inseridos
+# print('A soma entre ',  num1, 'e', num2, 'é', soma)
