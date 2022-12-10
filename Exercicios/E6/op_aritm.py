4967e281ced592851456903094796a330878eaac
wtbno <bruno_links@hotmail.com>
2022-12-10 13:15:35 -0300

    Tipos primitivos

---
+++ Exercicios/E6/op_aritm.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um número: '))
+
+dobro = numero + numero
+trip = numero * 3
+raiz = numero ** (1/2)
+print('O dobro desse número vale {} \n o triplo desse número vale {} \n e sua raiz quadrada vale {}' .format(numero, dobro, trip, raiz))
