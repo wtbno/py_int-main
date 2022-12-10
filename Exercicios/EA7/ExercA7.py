b2359d7dc19b51c7a3e4e2a0028eb2313c4a0d2d
wtbno <bruno_links@hotmail.com>
2022-12-10 12:06:21 -0300

    feat exercicios aula 7

---
+++ Exercicios/EA7/ExercA7.py
@@ -0,0 +1,8 @@
+n1 = int(input('Insira um número: '))
+n2 = int(input('Insira outro número: '))
+soma = n1 + n2
+mult = n1 * n2
+div = n1 / n2
+subt = n1 - n2
+elev = n1 ** n2
+print('A soma é {}, a multiplicação é: {}, a divisão é: {:.3f}, \n a subtração é: {}, a pontenciação é: {}' .format(soma, mult, div, subt, elev))
