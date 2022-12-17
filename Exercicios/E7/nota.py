c67b19967b21d4c05061390fe5cae09ba8bb7468
wtbno <bruno_links@hotmail.com>
2022-12-17 16:31:48 -0300

    Feat. nota de alunos

---
+++ Exercicios/E7/nota.py
@@ -0,0 +1,5 @@
+prova_1 = int(input('Insira a nota da sua primeira prova: '))
+prova_2 = int(input('Insira a nota da sua segunda prova: '))
+mediaFinal = (prova_1 + prova_2) / 2
+#Aplicar a ordem de precedencia
+print('A sua média é {}' .format(mediaFinal))
