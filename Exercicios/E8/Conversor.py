60367ba89a0940789e53e7d46a67356ea540e33a
wtbno <bruno_links@hotmail.com>
2022-12-17 18:04:02 -0300

    feat conversor de medidas

--- Exercicios/E8/Conversor.py
+++ Exercicios/E8/Conversor.py
@@ -1,6 +1,8 @@
 medida = float(input('Insira um valor para a ser convertido: '))
+km = medida * 1000
 centimetros = medida * 100
 milimetros = medida * 1000
-print('A medida de {}m em cm é {} e em mm é {}' .format(
+print('A a conversão de {}m metros em centimetros é de {}cm e em milimitros {}mm ' .format(
     medida, centimetros, milimetros))
+print('A conversão de {}km em metros é {}m' .format(medida, km))
 # km hm dam M dm cm mm
