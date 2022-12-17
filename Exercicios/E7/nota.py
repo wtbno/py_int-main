369219bf2f8d609bbd1c21bc5ae0346064f7410d
wtbno <bruno_links@hotmail.com>
2022-12-17 16:31:48 -0300

    Feat. nota de alunos

---
+++ Exercicios/E7/nota.py
@@ -0,0 +1,8 @@
+prova_1 = int(input('Insira a nota da sua primeira prova: '))
+prova_2 = int(input('Insira a nota da sua segunda prova: '))
+mediaFinal = (prova_1 + prova_2) / 2
+if mediaFinal >= 7
+    print('Aprovado')
+else:
+#Aplicar a ordem de precedencia
+print('A sua média final é {}' .format(mediaFinal))
