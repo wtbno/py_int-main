826b19113dbac6b4cf4821752802e8114a898f5a
wtbno <bruno_links@hotmail.com>
2022-12-20 20:03:54 -0300

    fix moeda

--- Exercicios/E10/Moeda.py
+++ Exercicios/E10/Moeda.py
@@ -1,8 +1,15 @@
+from tkinter import *
 real = float(input('Quanto dinheiro você possui na carteira? R$ '))
 dolares = real / 5.31
 euro = real / 5.63
-yene = real / 0.039
+yene = real / 25.34
 print('Com R$ {:.2f} você pode comprar U$ {:.2f}' .format(
     real, dolares))
 print('Com R$ {:.2f} você pode comprar € {:.2f}' .format(real, euro))
 print('Com R$ {:.2f} você pode comprar ¥ {:.2f}' .format(real, yene))
+
+
+janela = Tk()
+
+
+janela.mainloop
