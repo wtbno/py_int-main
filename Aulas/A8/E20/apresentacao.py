aaad159da92c2eef8568fc254a0367ad0ea8301f
wtbno <bruno_links@hotmail.com>
2022-12-21 20:25:04 -0300

    feat sorted

---
+++ Aulas/A8/E20/apresentacao.py
@@ -0,0 +1,7 @@
+aluno1 = str(input('Nome do primeiro aluno: '))
+aluno2 = str(input('Nome do segundo aluno: '))
+aluno3 = str(input('Nome do terceiro aluno: '))
+aluno4 = str(input('Nome do quarto aluno: '))
+lista = [aluno1, aluno2, aluno3, aluno4]
+escolhido = sorted(lista)
+print('A ordem de alunos é: {}' .format(escolhido))
