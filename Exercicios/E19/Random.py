39d511b8accbd44925e7095eecc9585c48e6856b
wtbno <bruno_links@hotmail.com>
2022-12-21 20:09:26 -0300

    feat. random

---
+++ Exercicios/E19/Random.py
@@ -0,0 +1,8 @@
+import random
+aluno1 = str(input('Primeiro aluno: '))
+aluno2 = str(input('Segundo aluno: '))
+aluno3 = str(input('Terceiro aluno: '))
+aluno4 = str(input('Quarto aluno: '))
+lista = [aluno1, aluno2, aluno3, aluno4]
+escolhido = random.choice(lista)
+print('O aluno escolhido foi: {} ' .format(escolhido))
