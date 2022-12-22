41e3a2149a3905117e33d189f9e23a67db644d95
wtbno <bruno_links@hotmail.com>
2022-12-21 21:16:59 -0300

    feat numeros aleatorios

---
+++ Aulas/A10/AE10/Numero.py
@@ -0,0 +1,8 @@
+import random
+aleatorio = random.randint(0, 10)
+escolha = int(input('Escolha um número de 0 a 10: '))
+if aleatorio == escolha:
+    print('Parabéns, você acertou')
+else:
+    print('Número incorreto!')
+print('O número escolhido foi: {}' .format(aleatorio))
