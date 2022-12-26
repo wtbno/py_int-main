6f8d769e340aee68aa89e124bfacb2a0a15b707a
wtbno <bruno_links@hotmail.com>
2022-12-26 16:41:02 -0300

    feat. emoji aula AE10

--- Aulas/A10/AE10/Numero.py
+++ Aulas/A10/AE10/Numero.py
@@ -1,9 +1,12 @@
 import random
 import emoji
+from time import sleep
 aleatorio = random.randint(0, 100)  # Faz o PC escolher um número
 print('-*-' * 20)
 escolha = int(input('Pense em um número de 0 a 100: '))
 print('-*-' * 20)
+print(emoji.emojize('Processando . . . :robot:'))
+sleep(3)
 if aleatorio == escolha:
     print(emoji.emojize('Parabéns, você venceu {} :celebration:' .format(aleatorio)))
 else:
