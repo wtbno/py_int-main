4e6aa6f1adf67930031ae78610b27de8a65310f0
wtbno <bruno_links@hotmail.com>
2022-12-26 17:14:26 -0300

    feat. resultado par ou impar

--- Aulas/A10/AE10c/ImparPar.py
+++ Aulas/A10/AE10c/ImparPar.py
@@ -1,3 +1,6 @@
 numero = int(input('Insira um número: '))
 resultado = numero % 2
-print('O resultado foi: {}' .format(resultado))
+if resultado == 0:
+    print('O número  {} é PAR ' .format(numero))
+if resultado == 1:
+    print('O número  {} é ÍMPAR ' .format(numero))
